a09d16c977a3e949fdfe6fc1dc72957d2720de3f
Jaja Harris <jajaharris@gmail.com>
2023-05-18 09:34:52 -0700

    add enter and exit full screen logic\

--- lib/services/layout_service.dart
+++ lib/services/layout_service.dart
@@ -1,1 +1,16 @@
-class LayoutService {}
+import 'package:stacked/stacked.dart';
+
+class LayoutService with ListenableServiceMixin {
+  bool _fullScreenMode = false;
+  bool get fullScreenMode => _fullScreenMode;
+
+  void enterFullScreen() {
+    _fullScreenMode = true;
+    notifyListeners();
+  }
+
+  void exitFullScreen() {
+    _fullScreenMode = false;
+    notifyListeners();
+  }
+}
